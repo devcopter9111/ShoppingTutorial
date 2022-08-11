407dcea3b51d9f73298718aacdcfaac2b67eab6b
dev1 <dev1@gmail.com>
2022-08-11 10:02:13 +0900

    complete

--- src/App.js
+++ src/App.js
@@ -1,31 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Home from './components/Home'
+import Cart from './components/Cart'
 
-function getReactVersion() {
-  return React.version;
-}
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        console.log(React.version);
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+       <BrowserRouter>
+            <div className="App">
+            
+              <Navbar/>
+                <Routes>
+                    <Route exact path="/" element={<Home/>}/>
+                    <Route path="/cart" element={<Cart/>}/>
+                  </Routes>
+             </div>
+       </BrowserRouter>
+      
+    );
+  }
 }
 
 export default App;
